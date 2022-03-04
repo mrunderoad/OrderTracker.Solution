dc35ec7a7f75983318d18d9943db25a86a8a3939
Isaac Overstreet <isaacoverstreet3@gmail.com>
2022-03-04 09:09:02 -0800

    return order price test pass

--- OrderTracker.Tests/ModelTests/OrderTests.cs
+++ OrderTracker.Tests/ModelTests/OrderTests.cs
@@ -17,7 +17,7 @@ namespace OrderTracker.TestTools
     }
 
     [TestMethod]
-    public void GetTitle_ReturnsOrderTitle_String()
+    public void GetOrderTitle_ReturnsOrderTitle_String()
     {
       string orderTitle = "testTitle";
       string orderDescription = "testDescription";
@@ -29,7 +29,7 @@ namespace OrderTracker.TestTools
     }
 
     [TestMethod]
-    public void GetTitle_ReturnsOrderDescription_String()
+    public void GetOrderDescription_ReturnsOrderDescription_String()
     {
       string orderTitle = "testTitle";
       string orderDescription = "testDescription";
@@ -38,6 +38,18 @@ namespace OrderTracker.TestTools
       Order newOrder = new Order(orderTitle, orderDescription, orderPrice, orderDate);
       string resultDescription = newOrder.OrderDescription;
       Assert.AreEqual(orderDescription, resultDescription);
+    }
+
+    [TestMethod]
+    public void GetOrderPrice_ReturnsOrderPrice_Int()
+    {
+      string orderTitle = "testTitle";
+      string orderDescription = "testDescription";
+      int orderPrice = 10;
+      string orderDate = "testDate";
+      Order newOrder = new Order(orderTitle, orderDescription, orderPrice, orderDate);
+      int resultPrice = newOrder.OrderPrice;
+      Assert.AreEqual(orderPrice, resultPrice);
     }
   }
 }
