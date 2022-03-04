216115c25e97aff8be43b301462cb63e88178c9d
Isaac Overstreet <isaacoverstreet3@gmail.com>
2022-03-04 09:43:29 -0800

    vendor creation test passing

---
+++ OrderTracker.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OrderTracker.Models;
+using System.Collections.Generic;
+using System;
+
+namespace OrderTracker.Tests
+{
+  [TestClass]
+  public class VandorTests //: IDisposable
+  {
+
+    // public void Dispose()
+    // {
+    //   Vendor.ClearAll();
+    // }
+
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("testName", "testDescription");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+  }
+}
---
+++ OrderTracker/Models/Vendor.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+
+namespace OrderTracker.Models
+{
+  public class Vendor
+  {
+    private static List<Vendor> _vendors = new List<Vendor> {};
+    public string VendorName { get; set; }
+    public string VendorDescription { get; set; }
+    public int Id { get; }
+    public List<Order> Orders { get; set; }
+
+    public Vendor(string vendorName, string vendorDescription)
+    {
+      VendorName = vendorName;
+      VendorDescription = vendorDescription;
+      _vendors.Add(this);
+      Id = _vendors.Count;
+      Orders = new List<Order> {};
+    }
+  }
+}
